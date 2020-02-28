659c4bec44a601e6564c20220c23d688b20cd4d4
Olaf Ideas <olaf.ideas@gmail.com>
2020-02-28 16:56:50 +0100

    Banner + trees

---
+++ banner.cpp
@@ -0,0 +1,43 @@
+/*
+
+                                                                                `````                                                                                     
+                                                                            `:ooyddddoo+.                       +ooooo:                                                   
+                                                                          `:ydddddddddddh+.                     hdddddo                                                   
+                 `+hhhh+shh/::                                           `+ddddddddddddddds.                    hdddddo                                                   
+               `+dmmNNmmmmmmmmh:                                         -dddddddsooodddddd+    ::yyyyyyyo.     hdddddo:yyyyyy/` `/yyyyy:   :yyyyyyyyo.                   
+             `+oNmNmhohmmsomNNmd/``     ````                             -ddddddd-`  ++++++-  :yhdddddddddho.   hdddddo-sdddddd` .dddddd:`:yhdddddddddho.                 
+            +mNmNNms:ooooo.+dmNmNNh: `/omNNmo/`                          -dddddddddo++``     :ydddddhhhdddddd-  hdddddo /hddddd.`odddddo.odddddhhhhdddddo`                
+            dNNmy/ohNmmmmmmNo+dNNmNh/ymNmmmNNmdssso:`                    `+ddddddddddddh+.`  +dddddh: -dddddds. hdddddo  +dddddh`dddddd/ odddddy  :dddddd.                
+            dmNNmhmmmmmmmmmmmyyNNmmNNNmNh::ymNmmmNNmh/`                   `/syddddddddddddy::ydddddh  `+dddddd- hdddddo  .sddddh:dddddo./hdddddhssydddddd.                
+           :dNNmd/NmmmmmmmmmmyNNmmdssyso/://os+-/hmmNN-                      `-/oohddddddddhhddddddh   :dddddd- hdddddo   oddddddddddd+ odddddddddddddddd.                
+           +mNNd/.yNNmmmmNNmo`:NNNy. :ymNNNNmdy- +NNNN+`                .yyyyyyy- `.+dddddddsydddddh  `+dddddd- hdddddo   `sdddddddddo` :ydddddh++++++++-`                
+           `dNNNmmhoymmmmdooommNNNh:ommmmmmmmNmmo:hmNmmd/               .hddddddo.``/ddddddh/+dddddh/`-ddddddo. hdddddo    :ydddddddh+   odddddy``:hhhhhh.                
+     `.....hmNmNmNmh//ooo-.mNNmmmy-oNmmmmmmmmmmNm+ :hNNmy                -ddddddddddddddddh/ -ydddddddddddddh-  hdddddo     sdddddddo    ohdddddddddddddh.                
+    -hNmmNmmmmhhydmNNNNNNmNmNNNmy. +mmmmmmmmmmNmm+.+dNNms                `/yddddddddddddho-   +hdddddddddddo.   hdddddo     -ydddddh+     ohddddddddddy/`                 
+   `+mNmhymmh:   .yddydmNmmNyNmmmdy+ymmNmmmmNNmms:ymNmh:`                  `/yddddddddho.      .oshdddddy+-.    ohdddh/      ohdddh+       -osddddddy/`                   
+   -hmNm/ ./hhhhhhs+: .yNNNN`-yNNmm+.+dmNNNNNds+`.sNmNd.                     `.........           .......        .....        .....           ......`                     
+  -dmNmmy+hmNmmmmmNNds-smmNN+:oNNNNs.:+-o+++o-++++yNmms`                                                                                                                  
+ .mNNmy:`smmmmmmmmmmNm+`ymmmmmNNmNmmmmmmm+`.mmNNNNmmms`        ``://////////:`                                                                                            
+ .NNmh-  smmmmmmmmmmmNm: .dNmNmddmNmddmNNmNNNmmddds+.         `+dddddddddddddy:`                                 `++++++-                                                 
+ .dNmNd/`smNmmmmmmmmNN/.omNmmmy-/hNNo-hNmmNNmh-              `+dddddddddddddddd:                    `......`     `dddddd/       ......`             `.....`  `....`       
+  .hmNNNy:smmmmmmmNmd+.sNmmyyohNNmmmNNNNyymNNm+              :ddddddd:--:ddddddysssssso   -ssssss`-:sddddddys/` osddddddyss- -osddddddys/`   `ssssssydddddssssddddys/`    
+   -mmmm/  .ossssss+.  smmNd`NmmmmmmmmmN.:mNNmy              :ddddddd/` `oooooo:/hddddho -ydddddsshdddddddddddy.hdddddddddd-ohdddddddddddy.  `dddddddddddddddddddddddy-   
+   `/NNNmdddd+:  .oddddmNNmm`mNmmmmmmNmm.dmmNh`              .sdddddddhyy/:      sdddddy :ddddddhhdddddddddddddhhddddddddds/yddddh+oddddddh. `dddddddddddddddddddddddd+`  
+    .ommmmmNmNmosmmNmmmmmmN.`mmNmmmmNmNo`+mNNmy               .sddddddddddhhs+-` :yddddh::dddddoyhdddddy:/ddhhh+``dddddd+``sdddddy``/dddddd+``dddddd+``odddddy`.odddddd.  
+       -///hmmmmNmmy///hNmmNdsodNNNNmdsosmNNNd:                .oyhdddddddddddd/` ohddddddddddh- ydddddddddds+-` `dddddd/ oddddddddddddddddd.`dddddd+  odddddy  :dddddd.  
+            ::yhhy-    -smmNNmh:oooo/+dNmNNmy:               `----/::ssddddddddd:  ydddddddddd:  -shddddddddddds``dddddd/ oddddddddddddddddy``dddddd+  odddddy  :dddddd.  
+                         -smNNNmNmNmmNmmmy:-                 :dddddy:` -:ddddddd:  ohddddddddy-    -:ssddddddddd:`dddddd/ oddddddhssssssss:` `dddddd+  odddddy  :dddddd.  
+                           .sNmmmmmmmNNy+`                   :ddddddd/``/ddddddd:   hdddddddd/  oyyyyyo..+ddddddo`dddddd/ .sdddddy  :yyyyyy+``dddddd+  odddddy  :dddddd.  
+                             ...``.....`                     :ddddddddhhddddddds.   `ddddddd+`  /hddddhhhhdddddd``dddddd/  :yddddhhhhdddddo` `dddddd+  odddddy  :dddddd.  
+                                                              -ohdddddddddddhh/      ddddddh/    /ydddddddddddh. `dddddd/   :yhdddddddddho.  `dddddd+  odddddy  :dddddd.  
+                                                                -oyhdddddhyo-        dddddd+       /yyddddhy+:`  `/yyyyo.     :yydddhyy+.    `yyyyyo.  +yyyyy: .+yyyyy+`  
+                                                                   -:::::-          oddddds.         `::::.                     `:::.                                     
+                                                                                   ohddddy/                                                                               
+                                                                                   yddddy/                                                                                
+                                                                                   ``````                                                                                
+	Team Pokor Fanclub Odzial A members:
+	> Olaf Surgut
+	> Wiktor Krzeminski
+	> Bartosz Brejna
+
+*/
