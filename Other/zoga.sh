66dc90fef9082f8376a10a7022a2ce3b660a26f7
Olaf Surgut <olaf.ideas@gmail.com>
2020-11-04 18:57:14 +0100

    zoga.sh

---
+++ Other/zoga.sh
@@ -0,0 +1,24 @@
+MEM_LIMIT=512 # in megabytes
+TIME_LIMIT=12 # in seconds
+for((i=1; ; i++)); do
+    echo -n "$i: "
+    ./gen $i > in
+    ./brute < in > ans
+    command time -f "%M %e %x" ./gan < in > out 2> log
+    read MEM TIME STATUS <log
+    MEM=$((MEM/1024))
+    echo -n "${MEM}/${MEM_LIMIT}Mb ${TIME}/${TIME_LIMIT}s "
+
+    if (( MEM >= MEM_LIMIT )); then  
+        echo -e "\e[93mmemory limit\e[39m"
+        break
+    elif (( $(echo "$TIME > $TIME_LIMIT" | bc -l) )); then
+        echo -e "\e[93mtime limit\e[39m"
+        break
+    elif ! diff -bwq ans out &> /dev/null; then
+        echo -e "\e[91mwrong answer\e[39m"
+        break
+    fi
+
+    echo -e "\e[92maccepted\e[39m"
+done
